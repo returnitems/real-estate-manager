4c3147629a238a40c93085b3e38b9fc465ab30ed
returnitems <diwan.umang@gmail.com>
2024-06-20 22:03:23 -0400

    Completed bootstrap styling

--- src/components/Properties/PropertyDeleteConf.jsx
+++ src/components/Properties/PropertyDeleteConf.jsx
@@ -1,14 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import "./PropertyDeleteConf.css";
 
 const PropertyDeleteConf = ({ selectedProperty, handleDeleteProperty }) => {
     const navigate = useNavigate();
     
     return (
-        <div>
-            <h2>Are you sure you want to delete "{selectedProperty.name}"?</h2>
-            <button onClick={() => handleDeleteProperty(selectedProperty._id)}>Delete</button>
+        <div className="container">
+            <h2 className="delete-confirmation">Are you sure you want to delete "{selectedProperty.name}"?</h2>
+            <button className="btn btn-danger" onClick={() => handleDeleteProperty(selectedProperty._id)}>Delete</button>
             {/* <button onClick={() => setView('propdetails')}>Cancel</button> */}
-            <button onClick={() => navigate(`/properties/${selectedProperty._id}`)}>Cancel Delete</button>
+            <button className="btn btn-success" onClick={() => navigate(`/properties/${selectedProperty._id}`)}>Cancel Delete</button>
         </div>
     );
 };
